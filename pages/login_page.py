fabfce1eced3701dbd94c6103be09bc13ff72f59
Ocman <ocman.cq@gmail.com>
2023-07-02 17:18:26 +0300

    organize PageObject model for pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,5 @@
-from selenium.webdriver.common.by import By
-
 from .base_page import BasePage
-from .locators import LoginPageLocators, MainPageLocators
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -9,9 +7,6 @@ class LoginPage(BasePage):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
-
-    #MainPageLocators.LOGIN_LINK
-
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
@@ -21,3 +16,14 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         assert "login" in self.browser.current_url, "'login' not in current url"
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL_FIELD).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.PASSWORD_FIELD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.PASSWORD_APROOVE_FIELD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_FORM_BUTTON).click()
+
+    def should_not_be_alert_register_form(self):
+        assert self.is_not_element_present(*LoginPageLocators.REGISTER_FORM_ALERT),\
+        "Error alert located in register form"
+
--- pages/product_page.py
+++ pages/product_page.py
@@ -3,7 +3,6 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-
     def add_product_to_cart(self):
         cart_button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
         cart_button.click()
@@ -13,7 +12,15 @@ class ProductPage(BasePage):
         product_title_hint  = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_HINT)
         assert product_title_page.text == product_title_hint.text, "product not in cart"
 
-    def should_be_equal_price_product_in_cart(self):
+    def should_be_equal_price_product(self):
         product_price_page= self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_PRODUCT_PAGE)
         product_price_hint  = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_HINT)
         assert product_price_page.text == product_price_hint.text, "price not equal"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared"
